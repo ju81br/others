333a89792748dbda1a0abd592d692ad332a19a70
Fonseca <sergio.car.fonseca@dxc.com>
2020-04-03 09:49:40 -0300

    finally

---
+++ java/java-pilha/src/TesteConexao.java
@@ -0,0 +1,22 @@
+
+public class TesteConexao {
+	public static void main(String[] args) {
+		try (Conexao conexao = new Conexao()){
+			conexao.leDados();
+		}catch (IllegalStateException ex) {
+			System.out.println("Deu pau na conexão");
+		}
+		
+		
+//		Conexao con = null;
+//		try {
+//			con = new Conexao();
+//			con.leDados();
+//			con.fecha();
+//		}catch(IllegalStateException ex) {
+//			System.out.println("Deu pau na conexão");
+//		}finally {
+//			con.fecha();
+//		}
+	}
+}
